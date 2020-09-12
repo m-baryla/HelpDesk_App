9e86904191547b6d280e41226c458c635ba27729
m-baryla <m-baryla@outlook.com>
2020-09-12 17:07:08 +0200

    Subject: Barcode and QR generator added in the UpdateNotebooksForms
    Version: v.0.0.1
    
    Comment: Added barcode and QR code generator and small form GUI
             fixes changed tab control
    
    Target: UpdateNotebooksForms
    
    Status: Correct working
    
    Tag: #feature
    
    The end...

--- GUI/Forms/UpdateNotebooksForms.cs
+++ GUI/Forms/UpdateNotebooksForms.cs
@@ -1,4 +1,6 @@
 ﻿using System;
+using System.Drawing;
+using System.Drawing.Drawing2D;
 using System.Linq;
 using System.Windows.Forms;
 using Interfaces;
@@ -123,6 +125,51 @@ namespace GUI.Forms
         {
             groupBoxAddNewUser.Visible = false;
         }
+        private void buttonCreateQR_Click(object sender, EventArgs e)
+        {
+            labelDateTimeCode.Text = DateTime.Now.ToString();
+            labelCompanyFixedCode.Text = textBoxCompanyFixedAssetNotebook.Text;
+            labelModelCode.Text = comboBoxModelNotebook.Text;
+            labelTahServiceCode.Text = textBoxTagServiceNotebook.Text;
+
+            Zen.Barcode.CodeQrBarcodeDraw qrBarcodeDraw = Zen.Barcode.BarcodeDrawFactory.CodeQr;
+            pictureBoxQRCode.Image = qrBarcodeDraw.Draw(textBoxCompanyFixedAssetNotebook.Text
+                                                        + " " +
+                                                        textBoxTagServiceNotebook.Text
+                                                        + " " +
+                                                        comboBoxModelNotebook.Text, pictureBoxQRCode.Width);
+
+            Zen.Barcode.Code128BarcodeDraw barcodeDraw = Zen.Barcode.BarcodeDrawFactory.Code128WithChecksum;
+            pictureBoxBarcode.Image = barcodeDraw.Draw(textBoxCompanyFixedAssetNotebook.Text
+                                                       + " " +
+                                                       textBoxTagServiceNotebook.Text
+                                                       + " " +
+                                                       comboBoxModelNotebook.Text, pictureBoxBarcode.Height);
+        }
+
+        private void pictureBoxQRCode_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox panel = (PictureBox)sender;
+            float width = (float)4.0;
+            Pen pen = new Pen(Color.DarkRed, width);
+            pen.DashStyle = DashStyle.DashDotDot;
+            e.Graphics.DrawLine(pen, 0, 0, 0, panel.Height - 0);
+            e.Graphics.DrawLine(pen, 0, 0, panel.Width - 0, 0);
+            e.Graphics.DrawLine(pen, panel.Width - 1, panel.Height - 1, 0, panel.Height - 1);
+            e.Graphics.DrawLine(pen, panel.Width - 1, panel.Height - 1, panel.Width - 1, 0);
+        }
+
+        private void pictureBoxBarcode_Paint(object sender, PaintEventArgs e)
+        {
+            PictureBox panel = (PictureBox)sender;
+            float width = (float)4.0;
+            Pen pen = new Pen(Color.DarkRed, width);
+            pen.DashStyle = DashStyle.DashDotDot;
+            e.Graphics.DrawLine(pen, 0, 0, 0, panel.Height - 0);
+            e.Graphics.DrawLine(pen, 0, 0, panel.Width - 0, 0);
+            e.Graphics.DrawLine(pen, panel.Width - 1, panel.Height - 1, 0, panel.Height - 1);
+            e.Graphics.DrawLine(pen, panel.Width - 1, panel.Height - 1, panel.Width - 1, 0);
+        }
         #endregion
 
         #region LABEL LINK ADD NEW --VALUES
@@ -175,6 +222,5 @@ namespace GUI.Forms
             comboBoxUsers.Text = textBoxFirstName.Text + " " + textBoxLastName.Text;
         }
         #endregion
-
     }
 }
